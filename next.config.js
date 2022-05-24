97580a1bb9a64a7cb773d5641cf425f5060614bb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-05-24 08:14:50 +0100

    enter pagination

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.experiments = { topLevelAwait: true };
+    return config;
+  },
+};
